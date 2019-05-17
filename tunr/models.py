921c948d3b6b9d68cc9ab29f0f7fb7bca996e880
dli24 <dli24@buffalo.edu>
2019-05-17 13:09:19 -0700

    DJANGO

---
+++ tunr/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    photo_url = models.TextField()
+    spotify = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+    title = models.CharField(max_length=100)
+    preview_url = models.TextField(null=True)
+    album = models.TextField(null=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.artist.name}'
